eba1065ed24bdeab669761899c7e9f1616b2c54f
Silvia Zhdanska <silviaalbert1992@gmail.com>
2024-08-17 21:19:58 +0300

    update styles

--- src/components/options/Options.jsx
+++ src/components/options/Options.jsx
@@ -1,3 +1,4 @@
+import styles from "./Options.module.css";
 export default function Options({
   good,
   neutral,
@@ -7,11 +8,11 @@ export default function Options({
   totalFeedback,
 }) {
   return (
-    <>
+    <div className={styles.buttonsContainer}>
       <button onClick={() => onUpdate("good")}>Good {good}</button>
       <button onClick={() => onUpdate("neutral")}>Neutral {neutral}</button>
       <button onClick={() => onUpdate("bad")}>Bad {bad}</button>
       {totalFeedback > 0 && <button onClick={onReset}>Reset</button>}
-    </>
+    </div>
   );
 }
